6a64ffaa11c9834d9a02b1608a8258d23b8af00d
Stefan Arnason <stefanh.arnason@outlook.com>
2022-03-09 17:56:37 +0100

    First commit

---
+++ work.ps1
@@ -0,0 +1,8 @@
+$urls = 
+"https://atea.service-now.com/", 
+"https://agent.puzzel.com", 
+"https://apps.bosit.no/vpn/index.html", 
+"https://citrix.cloud.com"
+Start-Process "C:\Users\stefa\OneDrive\Documents\Stefan (Jobb) - Chrome.lnk" -ArgumentList $urls
+
+[Environment]::Exit(1)
